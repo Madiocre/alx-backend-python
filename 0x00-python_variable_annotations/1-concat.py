9a13af75c7965bdf3c28d7be36aac20b68f76520
madiocre <wolvirex@gmail.com>
2024-07-04 18:44:44 +0300

    py var annot style + floor

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,5 +5,5 @@ concat with annotation
 
 
 def concat(str1: str, str2: str) -> str:
-    """  """
+    """ concats 2 strings """
     return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+floor with annotation
+"""
+
+
+def floor(n: float) -> int:
+    """ floors the float to int """
+    return int(n)
+
