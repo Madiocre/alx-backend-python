74ae9a1e015e75981bb86288af023d0a82355fb5
madiocre <wolvirex@gmail.com>
2024-07-08 21:01:11 +0300

    py async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Using corotines async and await to make a delay function
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """
+    Function to create a delay between 0
+    and a number inputed by user
+    """
+    rand_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(rand_delay)
+    return rand_delay
