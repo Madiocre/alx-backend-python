69640d62ec0528466027c10da6eba63ff5428da1
madiocre <wolvirex@gmail.com>
2024-07-08 23:52:44 +0300

    finished task #1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """ Writes multiple corotine delays concurrently """
     lis_current = [wait_random(max_delay) for _ in range(n)]
     lis_fin = asyncio.as_completed(lis_current)
